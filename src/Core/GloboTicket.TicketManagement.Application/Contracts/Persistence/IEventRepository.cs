98c1d442f6b19f3a274aaf0e6c9cf22f45460735
Moshen <mehsen2222@gmail.com>
2024-09-06 23:07:07 +0300

    add validation to create event

--- src/Core/GloboTicket.TicketManagement.Application/Contracts/Persistence/IEventRepository.cs
+++ src/Core/GloboTicket.TicketManagement.Application/Contracts/Persistence/IEventRepository.cs
@@ -2,5 +2,7 @@ using GloboTicket.TicketManagement.Domain.Entities;
 
 namespace GloboTicket.TicketManagement.Application.Contracts.Persistence
 {
-    public interface IEventRepository : IAsyncRepository<Event> { }
+    public interface IEventRepository : IAsyncRepository<Event> { 
+        Task<bool> IsEventNameAndDateUnique(string name, DateTime eventDate);
+    }
 }
--- src/Core/GloboTicket.TicketManagement.Application/Features/Events/Command/CreateEvent/CreateEventCommandHandler.cs
+++ src/Core/GloboTicket.TicketManagement.Application/Features/Events/Command/CreateEvent/CreateEventCommandHandler.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Application.Exceptions;
 using GloboTicket.TicketManagement.Domain.Entities;
 using MediatR;
 
@@ -19,6 +20,13 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Command.Creat
         public async Task<Guid> Handle(CreateEventCommand request, CancellationToken cancellationToken)
         {
             var @event = _mapper.Map<Event>(request);
+
+            var validator = new CreateEventCommandValidator(_eventRepository);
+            var validationResult = await validator.ValidateAsync(request);
+            if (validationResult.Errors.Count > 0)
+            {
+                throw new ValidationException(validationResult);
+            }
 
             @event = await _eventRepository.AddAsync(@event);
 
