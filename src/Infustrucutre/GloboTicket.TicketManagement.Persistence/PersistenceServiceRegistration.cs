c5477fca534ed30de4978049c8440af0d5ead28f
Moshen <mehsen2222@gmail.com>
2024-09-10 13:48:40 +0300

    fix migration conflict issue

--- src/Infustrucutre/GloboTicket.TicketManagement.Persistence/PersistenceServiceRegistration.cs
+++ src/Infustrucutre/GloboTicket.TicketManagement.Persistence/PersistenceServiceRegistration.cs
@@ -11,7 +11,7 @@ namespace GloboTicket.TicketManagement.Persistence
         public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<GloboTicketDbContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("GloboTicketTicketManagementConnectionString")));
+                options.UseSqlServer(b => b.MigrationsAssembly("GloboTicket.TicketManagement.Api")));
 
             services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
 
