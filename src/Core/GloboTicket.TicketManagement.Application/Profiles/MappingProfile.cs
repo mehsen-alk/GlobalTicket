f1c9f4d633d598d7cca1f1d11db8e31aff77fccb
Moshen <mehsen2222@gmail.com>
2024-09-13 16:36:50 +0300

    add missing mapping profile

--- src/Core/GloboTicket.TicketManagement.Application/Profiles/MappingProfile.cs
+++ src/Core/GloboTicket.TicketManagement.Application/Profiles/MappingProfile.cs
@@ -1,5 +1,6 @@
 using System;
 using AutoMapper;
+using GloboTicket.TicketManagement.Application.Features.Categories.Commands.CreateCategory;
 using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
 using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesListWithEvents;
 using GloboTicket.TicketManagement.Application.Features.Events;
@@ -17,13 +18,15 @@ namespace GloboTicket.TicketManagement.Application.Profiles
         {
             CreateMap<Event, EventListVM>().ReverseMap();
             CreateMap<Event, EventDetailVM>().ReverseMap();
-            CreateMap<Category, CategoryDto>();
             CreateMap<Event, CreateEventCommand>().ReverseMap();
             CreateMap<Event, UpdateEventCommand>().ReverseMap();
             CreateMap<Event, DeleteEventCommand>().ReverseMap();
 
+            CreateMap<Category, CategoryDto>();
             CreateMap<Category, CategoryListVm>();
             CreateMap<Category, CategoryEventListVm>();
+            CreateMap<Category, CreateCategoryCommand>();
+            CreateMap<Category, CreateCategoryDto>();
         }
     }
 }
